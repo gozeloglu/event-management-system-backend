5900a9834fe03e0ba0d918837a8b55d6f7176734
gozeloglu <gozeloglu@gmail.com>
2020-07-21 19:45:40 +0300

    some files are added

---
+++ src/test/java/eventmanagementinternyte/eventmanagementsystembackend/EventManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eventmanagementinternyte.eventmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
